19a9481d5a6eb1ed7da763acd298fa164e6b54f2
Ronald Tse <ronald.tse@ribose.com>
2020-11-20 13:36:09 +0800

    Fix spec for using string IDs

--- spec/iev/termbase/workbook_spec.rb
+++ spec/iev/termbase/workbook_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Iev::Termbase::Workbook do
     it "parses to xls file to workbook" do
       workbook = Iev::Termbase::Workbook.parse(sample_file)
 
-      first_term = workbook.to_hash[1030101]
+      first_term = workbook.to_hash["103-01-01"]
       kor_source = first_term["kor"]["authoritative_source"]
 
       expect(workbook.size).to eq(2)
-      expect(first_term["eng"]["id"]).to eq(1030101)
+      expect(first_term["eng"]["id"]).to eq("103-01-01")
       expect(kor_source["clause"]).to eq("103-08-10")
       expect(kor_source["link"]).to eq("https://webstore.iec.ch/publication/161")
     end
