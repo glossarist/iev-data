851d5bac7d4b02a9f61f48a54e0bfe9e9765398f
Ronald Tse <ronald.tse@ribose.com>
2020-12-16 15:59:21 +0800

    Handle non 2-char LANGUAGE codes, fixes #41

--- lib/iev/termbase/iso_639_code.rb
+++ lib/iev/termbase/iso_639_code.rb
@@ -6,13 +6,25 @@ module Iev
       COUNTRY_CODES = YAML.load(IO.read(File.join(__dir__, "iso_639_2.yaml")))
 
       def initialize(two_char_code)
-        @code = two_char_code
+        @code = case two_char_code.length
+        when 2
+          two_char_code
+        else
+          # This is to handle code "nl BE" in the IEV sheet
+          two_char_code.split(" ").first
+        end
       end
 
       def find(code_type)
-        country_codes.detect do |key, value|
+        code = country_codes.detect do |key, value|
           key if value["iso_639_1"] == @code.to_s && value[code_type]
         end
+
+        if code.nil?
+          raise StandardError.new("Iso639Code not found for '#{@code}'!")
+        end
+
+        code
       end
 
       def self.three_char_code(two_char_code, code_type="terminology")
