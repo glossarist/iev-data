8299995b8d37a5ba3f0411854422dc51ff291aa4
Sebastian Skałacki <skalee@gmail.com>
2021-01-03 10:20:11 +0100

    Fix "gender" & "plurality" term attribute parsing

---
+++ spec/iev/termbase/terms_attrs_parser_spec.rb
@@ -0,0 +1,87 @@
+RSpec.describe Iev::Termbase::TermAttrsParser do
+  # Parses :string metadata or example description.
+  subject do
+    example = RSpec.current_example
+    attributes_str = example.metadata[:string] || example.description
+    described_class.new(attributes_str)
+  end
+
+  describe "gender" do
+    example "f" do
+      expect(subject.gender).to eq("f")
+    end
+    example "m" do
+      expect(subject.gender).to eq("m")
+    end
+    example "n" do
+      expect(subject.gender).to eq("n")
+    end
+
+    it "works for empty strings", string: "" do
+      expect(subject.gender).to be(nil)
+    end
+
+    it "works for strings which do not specify gender", string: "a whatever" do
+      expect(subject.gender).to be(nil)
+    end
+
+    it "allows comma as a separator", string: "m, whatever" do
+      expect(subject.gender).to eq("m")
+    end
+
+    it "is not fooled with longer sentences (begin/end of word)",
+      string: "noun" do
+      expect(subject.gender).to be(nil)
+    end
+
+    pending "it cannot be placed inside brackets" # too difficult for now
+  end
+
+  describe "plurality" do
+    example "pl" do
+      expect(subject.plurality).to eq("plural")
+    end
+
+    example "f pl" do
+      expect(subject.plurality).to eq("plural")
+    end
+    example "m pl" do
+      expect(subject.plurality).to eq("plural")
+    end
+    example "n pl" do
+      expect(subject.plurality).to eq("plural")
+    end
+
+    it "works for empty strings", string: "" do
+      expect(subject.plurality).to be(nil)
+    end
+
+    it "works for strings which do not specify plurality",
+      string: "a whatever" do
+      expect(subject.plurality).to be(nil)
+    end
+
+    it "allows comma as a separator", string: "m,pl" do
+      expect(subject.plurality).to eq("plural")
+    end
+
+    it "is not fooled with longer sentences (begin/end of word)",
+      string: "what a plapl" do
+      expect(subject.plurality).to be(nil)
+    end
+
+    it "always sets plurality if gender is specified", string: "f" do
+      expect(subject.plurality).to eq("singular")
+    end
+
+    it "always sets plurality if gender is specified", string: "m" do
+      expect(subject.plurality).to eq("singular")
+    end
+
+    it "always sets plurality if gender is specified", string: "n" do
+      expect(subject.plurality).to eq("singular")
+    end
+
+    pending "it cannot be placed inside brackets" # too difficult for now
+  end
+end
