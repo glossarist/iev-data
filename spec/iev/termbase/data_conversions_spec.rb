d239d058b8ac098b9c6b5c1d5a85835ebc6915f7
Sebastian Skałacki <skalee@gmail.com>
2021-01-09 10:27:16 +0100

    Extract String#decode_html
    Decodes HTML entities with HTMLEntities gem.

--- spec/iev/termbase/data_conversions_spec.rb
+++ spec/iev/termbase/data_conversions_spec.rb
@@ -3,6 +3,26 @@ require "spec_helper"
 RSpec.describe "string conversion refinements" do
   using Iev::Termbase::DataConversions
 
+  describe "#decode_html" do
+    it "decodes HTML entities" do
+      str = "&lt;b&gt;what a&lt;/b&gt; string &&amp; stuff"
+      expect(str.decode_html).to eq("<b>what a</b> string && stuff")
+    end
+
+    it "returns decoded string but leaves self unchanged" do
+      str = "&lt;&gt;"
+      expect { str.decode_html }.not_to change { str }
+      expect(str.decode_html).not_to eq(str)
+    end
+  end
+
+  describe "#decode_html!" do
+    it "decodes string in place" do
+      str = "&lt;&gt;"
+      expect { str.decode_html! }.to change { str }.to("<>")
+    end
+  end
+
   describe "#sanitize" do
     it "strips leading and trailing white spaces" do
       str = "  whatever\t"
