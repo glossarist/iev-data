4696349415bc927e3eba906aaa113f439f817f35
Sebastian Skałacki <skalee@gmail.com>
2021-01-09 09:17:09 +0100

    Improve capturing stdout and stderr in specs
    This program prints some information to stdout and stderr. Proper stdout
    and stderr capturing is needed to get readable test results.  Also, some
    tests make expectations on what was written to stdout.
    
    This commit improves ease of use and quality of stdout and stderr
    capturing.

--- spec/acceptance/xlsx2yaml_spec.rb
+++ spec/acceptance/xlsx2yaml_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "IEV Termbase" do
   describe "xlsx2yaml" do
     it "parses xlsx document to yaml" do
       command = %W(xlsx2yaml #{sample_xlsx_file} -o ./tmp --no-write)
-      output = capture_stdout { Iev::Termbase::Cli.start(command) }
+      output, * = capture_output_streams { Iev::Termbase::Cli.start(command) }
 
       expect(output).to include("deu:")
       expect(output).to include("103-01-01:")
--- spec/support/console_helper.rb
+++ spec/support/console_helper.rb
@@ -1,29 +1,31 @@
 module Iev
   module ConsoleHelper
-    def capture_stdout(&_block)
+    # Executes given block.  Anything written to $stdout or $stderr in that
+    # block is captured and returned.
+    #
+    # @return Array - a three-element array composed of captured stdout,
+    #   captured stderr, and block's return value
+    def capture_output_streams
       original_stdout = $stdout
-      $stdout = fake = StringIO.new
+      original_stderr = $stderr
+      $stdout = fake_stdout = StringIO.new
+      $stderr = fake_stderr = StringIO.new
 
       begin
-        yield
+        block_return_value = yield
+        [fake_stdout.string, fake_stderr.string, block_return_value]
       ensure
         $stdout = original_stdout
-      end
-
-      fake.string
-    end
-
-    def capture_stderr(&_block)
-      original_stderr = $stderr
-      $stderr = fake = StringIO.new
-
-      begin
-        yield
-      ensure
         $stderr = original_stderr
       end
+    end
 
-      fake.string
+    # Executes given block.  Anything written to $stdout or $stderr in that
+    # block is suppressed.
+    #
+    # @return Object - block's return value
+    def silence_output_streams(&block)
+      capture_output_streams(&block)[2]
     end
   end
 end
