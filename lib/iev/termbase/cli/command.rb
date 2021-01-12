fd80bbcf3ae34ea5d2e5aae484bb7f0d6034e99a
Sebastian Skałacki <skalee@gmail.com>
2021-01-12 17:39:19 +0100

    Define xlsx2db and db2yaml commands

--- lib/iev/termbase/cli/command.rb
+++ lib/iev/termbase/cli/command.rb
@@ -23,6 +23,22 @@ module Iev::Termbase
         end
       end
 
+      desc "xlsx2db FILE, DB_FILE", "Imports Excel to SQLite database."
+      def xlsx2db(file, dbfile)
+        # Instantiating an in-memory db and dumping it later is faster than
+        # just working on file db.
+        db = Sequel.sqlite
+        DbWriter.new(db).import_spreadsheet(file)
+        save_db_to_file(db, dbfile)
+      end
+
+      desc "db2yaml DB_FILE", "Exports SQLite to IEV YAMLs."
+      def db2yaml(dbfile)
+        db = Sequel.sqlite(dbfile)
+        collection = ConceptCollection.build_from_dataset(db[:concepts])
+        write_to_file(file, collection, options)
+      end
+
       private
 
       def write_to_file(file, collection, options)
@@ -34,6 +50,18 @@ module Iev::Termbase
 
         collection.each do |key, concept|
           concept.to_file(concept_dir.join("concept-#{key}.yaml"))
+        end
+      end
+
+      # Note: Implementation examples here:
+      # https://www.rubydoc.info/github/luislavena/sqlite3-ruby/SQLite3/Backup
+      def save_db_to_file(src_db, dbfile)
+        puts "Saving database to file..."
+        src_db.synchronize do |src_conn|
+          dest_conn = SQLite3::Database.new(dbfile)
+          b = SQLite3::Backup.new(dest_conn, "main", src_conn, "main")
+          b.step(-1)
+          b.finish
         end
       end
 
