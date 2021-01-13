d2e36c79dfa6da42771db7c356246e70c22cdd13
Sebastian Skałacki <skalee@gmail.com>
2021-01-13 19:51:26 +0100

    Provide easy access to fixtures in tests
    A re-usable helper method which returns path to fixture of given name.

---
+++ spec/support/fixture_helper.rb
@@ -0,0 +1,11 @@
+module Iev
+  module FixtureHelper
+    def fixture_path(fixture_name)
+      File.expand_path(fixture_name, fixture_root)
+    end
+
+    def fixture_root
+      File.expand_path("../fixtures", __dir__)
+    end
+  end
+end
