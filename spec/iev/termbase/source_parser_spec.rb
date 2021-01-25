6afb22f76aa03beae3b11755f77fae15389a3df9
Sebastian Skałacki <skalee@gmail.com>
2021-01-25 18:15:38 +0100

    Use better matchers in SourceParser specs
    Simplify specs for SourceParser by using proper matchers instead of
    complex logic.

--- spec/iev/termbase/source_parser_spec.rb
+++ spec/iev/termbase/source_parser_spec.rb
@@ -8,79 +8,37 @@ RSpec.describe Iev::Termbase::SourceParser do
   end
 
   example "MOD,ITU", string: "702-01-02 MOD,ITU-R Rec. 431 MOD" do
-    results = subject.src_split
-
-    expect(results.class).to be(Array)
-    expect(results.size).to eq(2)
-
-    results.each do |r|
-      expect([ "702-01-02 MOD", "ITU-R Rec. 431 MOD" ]).to include(r)
-    end
+    expect(subject.src_split).to be_an(Array) & contain_exactly(
+      "702-01-02 MOD", "ITU-R Rec. 431 MOD")
   end
 
   example "MOD. ITU", string: "161-06-01 MOD. ITU RR 139 MOD" do
-    results = subject.src_split
-
-    expect(results.class).to be(Array)
-    expect(results.size).to eq(2)
-
-    results.each do |r|
-      expect([ "161-06-01 MOD", "ITU RR 139 MOD" ]).to include(r)
-    end
+    expect(subject.src_split).to be_an(Array) & contain_exactly(
+      "161-06-01 MOD", "ITU RR 139 MOD")
   end
 
   example "XXX-XX-XX, ITU", string: "725-12-50, ITU RR 11" do
-    results = subject.src_split
-
-    expect(results.class).to be(Array)
-    expect(results.size).to eq(2)
-
-    results.each do |r|
-      expect([ "725-12-50", "ITU RR 11" ]).to include(r)
-    end
+    expect(subject.src_split).to be_an(Array) & contain_exactly(
+      "725-12-50", "ITU RR 11")
   end
 
   example "XXX-XX-XX, YYY-YY-YY", string: "705-02-01, 702-02-07" do
-    results = subject.src_split
-
-    expect(results.class).to be(Array)
-    expect(results.size).to eq(2)
-
-    results.each do |r|
-      expect([ "705-02-01", "702-02-07" ]).to include(r)
-    end
+    expect(subject.src_split).to be_an(Array) & contain_exactly(
+      "705-02-01", "702-02-07")
   end
 
   example "702-09-44 MOD, 723-07-47, voir 723-10-91" do
-    results = subject.src_split
-
-    expect(results.class).to be(Array)
-    expect(results.size).to eq(3)
-
-    results.each do |r|
-      expect([ "702-09-44 MOD", "723-07-47", "voir 723-10-91" ]).to include(r)
-    end
+    expect(subject.src_split).to be_an(Array) & contain_exactly(
+      "702-09-44 MOD", "723-07-47", "voir 723-10-91")
   end
 
   example "IEC 62303:2008, 3.1, modified and IEC 62302:2007, 3.2; IAEA 4" do
-    results = subject.src_split
-
-    expect(results.class).to be(Array)
-    expect(results.size).to eq(3)
-
-    results.each do |r|
-      expect([ "IEC 62303:2008, 3.1, modified", "IEC 62302:2007, 3.2", "IAEA 4" ]).to include(r)
-    end
+    expect(subject.src_split).to be_an(Array) & contain_exactly(
+      "IEC 62303:2008, 3.1, modified", "IEC 62302:2007, 3.2", "IAEA 4")
   end
 
   example "CEI 62303:2008, 3.1, modifiée et CEI 62302:2007, 3.2; AIEA 4" do
-    results = subject.src_split
-
-    expect(results.class).to be(Array)
-    expect(results.size).to eq(3)
-
-    results.each do |r|
-      expect([ "CEI 62303:2008, 3.1, modifiée", "CEI 62302:2007, 3.2", "AIEA 4" ]).to include(r)
-    end
+    expect(subject.src_split).to be_an(Array) & contain_exactly(
+      "CEI 62303:2008, 3.1, modifiée", "CEI 62302:2007, 3.2", "AIEA 4")
   end
 end
