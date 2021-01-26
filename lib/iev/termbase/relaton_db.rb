bbe2d92351d9c9077c704fdb28043343e6810a28
Sebastian Skałacki <skalee@gmail.com>
2021-01-26 16:42:25 +0100

    Retry on Relaton errors, typically network errors

--- lib/iev/termbase/relaton_db.rb
+++ lib/iev/termbase/relaton_db.rb
@@ -13,7 +13,28 @@ module Iev
       # @param code [String] reference
       # @return [RelatonIso::IsoBibliongraphicItem]
       def fetch(code)
-        @db.fetch code
+        retrying_on_failures do
+          @db.fetch code
+        end
+      end
+
+      private
+
+      def retrying_on_failures(attempts: 4)
+        curr_attempt = 1
+
+        begin
+          yield
+
+        rescue
+          if curr_attempt <= attempts
+            sleep(2 ** curr_attempt * 0.1)
+            curr_attempt += 1
+            retry
+          else
+            raise
+          end
+        end
       end
     end
   end
