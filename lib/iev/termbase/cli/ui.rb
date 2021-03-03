646d93547898764629af2874f41c698249dc5cfe
Sebastian Skałacki <skalee@gmail.com>
2021-03-04 00:19:56 +0100

    Split UI methods
    Split CLI::UI#progress into simpler ones #progress, #info,
    and Helper#clear_progress.

--- lib/iev/termbase/cli/ui.rb
+++ lib/iev/termbase/cli/ui.rb
@@ -9,10 +9,23 @@ module IEV
       module UI
         module_function
 
-        def progress(message, persistent: false)
+        # Prints progress message which will be replaced on next call.
+        def progress(message)
           return unless $TERMBASE_PROGRESS
-          print "\r#{" " * 40}\r" # clear line
-          print persistent ? "#{message}\n" : "#{message} "
+          print "#{Helper.clear_progress}#{message} "
+        end
+
+        # Prints generic message.
+        def info(message)
+          print "#{Helper.clear_progress}#{message}\n"
+        end
+
+        module Helper
+          module_function
+
+          def clear_progress
+            $TERMBASE_PROGRESS ? "\r#{" " * 40}\r" : ""
+          end
         end
       end
     end
