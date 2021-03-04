207031cd58de69aff1e713de5666c90b7ebabc39
Sebastian Skałacki <skalee@gmail.com>
2021-03-04 04:14:24 +0100

    Use CLI::UI#debug, #info and #warn everywhere
    Replace all the Kernel#puts calls with these new methods.

--- lib/iev/termbase/concept.rb
+++ lib/iev/termbase/concept.rb
@@ -28,7 +28,7 @@ module IEV::Termbase
       if self[DEFAULT_LANGUAGE]
         self[DEFAULT_LANGUAGE]
       else
-        puts "[tc211-termbase] term (lang: #{keys.first}, ID: #{id}) is missing a corresponding English term, probably needs updating."
+        CLI::UI.warn "[tc211-termbase] term (lang: #{keys.first}, ID: #{id}) is missing a corresponding English term, probably needs updating."
         self[keys.first]
       end
     end
